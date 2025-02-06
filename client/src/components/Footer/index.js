7a0cdd69545213c6fafa2fcda1e47216db5e7902
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-02-06 16:36:08 -0300

    att

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import logoGovPE from '../../assets/logoGovPE.png'
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src={logoGovPE} alt="Logo do Governo do Estado de Pernambuco" />
+        </footer>
+    )
+}
+
+export default Footer
